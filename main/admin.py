6cf0d009b38a9fd410605ed36e90a90fd3b55954
Musa Kavus <musakavus@gmail.com>
2023-05-21 16:57:24 +0300

    kategori backend düzenlendi

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.utils.text import slugify
+from .models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent_category', 'created_at', 'updated_at')
+    list_filter = ('parent_category', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category_images/')
+    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE,
+                                        related_name='sub_categories')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, blank=True)  # Slug alanını ekleyin
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # Slug alanını otomatik olarak oluşturun
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
